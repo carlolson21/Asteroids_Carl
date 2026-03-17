5c45189e14b4bbf90ca56c1d4c6e749f8a60b4a1
carlolson21 <39631324+carlolson21@users.noreply.github.com>
2026-03-16 20:22:11 -0500

    B: Added Scoring and Game Over Screen

---
+++ gameover.py
@@ -0,0 +1,14 @@
+import pygame
+from constants import *
+
+def draw_pause_message(screen):
+    # Create a dark transparent overlay
+    overlay = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+    overlay.set_alpha(128) # 0 is clear, 255 is solid
+    overlay.fill((0, 0, 0))
+    screen.blit(overlay, (0,0))
+
+    font = pygame.font.Font(None, 74)
+    text = font.render("GAME OVER", True, "white")
+    text_rect = text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+    screen.blit(text, text_rect)
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+from score import *
+from gameover import *
 
 def main():
 	pygame.init()
@@ -21,37 +23,51 @@ def main():
 	AsteroidField.containers = (updatable)
 	Shot.containers = (shots, updatable, drawable)
 	
+	score_board = Score(10, 10)
+	updatable.add(score_board)
+	drawable.add(score_board)
+	
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	AsteroidField()
 	
 	dt = 0
+	running = True
+	game_over = False
+
 	
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	
-	while True:
+	while running:
 		log_state()
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				return
+				running = False
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					running = False
 		screen.fill("black")
 		for item in drawable:
 			item.draw(screen)
+		if game_over == True:
+			draw_pause_message(screen)
 		pygame.display.flip()
-		dt = clock.tick(60) / 1000
-		updatable.update(dt)
-		for asteroid in asteroids:
-			if player.collides_with(asteroid):
-				log_event("player_hit")
-				print("Game over!")
-				sys.exit()
-			for shot in shots:
-				if shot.collides_with(asteroid):
-					log_event("asteroid_shot")
-					shot.kill()
-					asteroid.split()
-
+		if game_over == False:
+			dt = clock.tick(60) / 1000
+			updatable.update(dt)
+			for asteroid in asteroids:
+				if player.collides_with(asteroid):
+					log_event("player_hit")
+					print("Game over!")
+					print(f"Score: {score_board.value}")
+					game_over = True
+				for shot in shots:
+					if shot.collides_with(asteroid):
+						log_event("asteroid_shot")
+						shot.kill()
+						score_board.value += 1
+						asteroid.split()
 
 if __name__ == "__main__":
     main()
